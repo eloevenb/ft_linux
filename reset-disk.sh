9a5f1b922c7ae86fe647bc2687f2b3707e5ab04b
eloevenb <eloevenb@student.42.fr>
2025-12-30 23:01:04 +0100

    reset disk

---
+++ reset-disk.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e 
+
+NC='\033[0m'
+DISK="/dev/sda"
+
+fuser -k ${DISK}* 2>/dev/null || true
+umount ${DISK}* 2>/dev/null || true
+sleep 1
+dd if=/dev/zero of=$DISK bs=512 count=1
+swapoff /dev/sda3
+partprobe $DISK
+
+rm -rf /mnt/lfs
