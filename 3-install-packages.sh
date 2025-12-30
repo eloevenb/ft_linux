870d863a7fe302b2090cda2306cce92424a9cb7f
eloevenb <eloevenb@student.42.fr>
2025-12-31 00:34:24 +0100

    install packages.sh

--- 3-install-packages.sh
+++ 3-install-packages.sh
@@ -3,6 +3,7 @@
 set -e
 
 LFS=/mnt/lfs
+LFS_TGT=$(uname -m)-eloevenb
 export MAKEFLAGS='-j$(nproc)'
 cd $LFS/sources
 
@@ -23,17 +24,82 @@ clean_up() {
 	fi
 }
 
+echo -e "${GREEN}[Binutils installed]${NC}"
+
 echo -e "${GREEN}[Installing packages]${NC}"
 # Binutils
-tar -xf binutils-2.30.tar.xz
-cd binutils-2.30
-create_build_dir
-../configure --prefix=/tools            \
-             --with-sysroot=$LFS        \
-             --with-lib-path=/tools/lib \
-             --target=$LFS_TGT          \
-             --disable-nls              \
-             --disable-werror
-make
-make install
-clean_up binutils-2.30
+{
+	tar -xf binutils-2.30.tar.xz > /dev/null 2>&1 && \
+	cd binutils-2.30 && \
+	create_build_dir > /dev/null 2>&1 && \
+	../configure --prefix=/tools \
+		--with-sysroot=$LFS \
+		--with-lib-path=/tools/lib \
+		--target=$LFS_TGT \
+		--disable-nls \
+		--disable-werror > /dev/null 2>&1 && \
+	make > /dev/null 2>&1 && \
+	make install > /dev/null 2>&1 && \
+	clean_up binutils-2.30
+} && \
+echo -e "${GREEN}[Binutils installed]${NC}" \
+|| echo -e "${RED}[Binutils build FAILED]${NC}"
+
+# Cross GCC
+{
+	tar -xf gcc-7.3.0.tar.xz > /dev/null 2>&1 && \
+	cd gcc-7.3.0 && \
+	tar -xf ../mpfr-4.0.1.tar.xz > /dev/null 2>&1 && \
+	mv -v mpfr-4.0.1 mpfr && \
+	tar -xf ../gmp-6.1.2.tar.xz > /dev/null 2>&1 && \
+	mv -v gmp-6.1.2 gmp && \
+	tar -xf ../mpc-1.1.0.tar.gz > /dev/null 2>&1 && \
+	mv -v mpc-1.1.0 mpc && \
+	create_build_dir > /dev/null 2>&1 && \
+	for file in gcc/config/{linux,i386/linux{,64}}.h
+		do
+		cp -uv $file{,.orig}
+		sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
+		-e 's@/usr@/tools@g' $file.orig > $file
+		echo '
+		#undef STANDARD_STARTFILE_PREFIX_1
+		#undef STANDARD_STARTFILE_PREFIX_2
+		#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
+		#define STANDARD_STARTFILE_PREFIX_2 ""' >> $file
+		touch $file.orig
+	done
+	case $(uname -m) in
+	x86_64)
+		sed -e '/m64=/s/lib64/lib/' \
+			-i.orig gcc/config/i386/t-linux64
+	;;
+	esac
+	create_build_dir > /dev/null 2>&1 && \
+	../configure \
+		--target=$LFS_TGT \
+		--prefix=/tools \
+		--with-glibc-version=2.11 \
+		--with-sysroot=$LFS \
+		--with-newlib \
+		--without-headers \
+		--with-local-prefix=/tools \
+		--with-native-system-header-dir=/tools/include \
+		--disable-nls \
+		--disable-shared \
+		--disable-multilib \
+		--disable-decimal-float \
+		--disable-threads \
+		--disable-libatomic \
+		--disable-libgomp \
+		--disable-libmpx \
+		--disable-libquadmath \
+		--disable-libssp \
+		--disable-libvtv \
+		--disable-libstdcxx \
+		--enable-languages=c,c++ > /dev/null 2>&1 && \
+	make > /dev/null 2>&1 && \
+	make install > /dev/null 2>&1 && \
+	clean_up gcc-7.3.0
+} && \
+echo -e "${GREEN}[GCC installed]${NC}" \
+|| echo -e "${RED}[GCC build FAILED]${NC}"
