8d4d5e1a118cdbc4360c3f837193a4b908ef8b00
eloevenb <eloevenb@student.42.fr>
2025-12-31 01:31:01 +0100

    0-version-check.sh

--- 0-version-check.sh
+++ 0-version-check.sh
@@ -27,8 +27,7 @@ echo awk is `/usr/bin/awk --version | head -n1`
 else
 echo "awk not found"
 fi
-Linux From Scratch - Version 8.2
-14
+
 gcc --version | head -n1
 g++ --version | head -n1
 ldd --version | head -n1 | cut -d" " -f2- # glibc version
@@ -43,8 +42,7 @@ sed --version | head -n1
 tar --version | head -n1
 makeinfo --version | head -n1
 xz --version | head -n1
-Linux From Scratch - Version 8.2
-15
+
 echo 'int main(){}' > dummy.c && g++ -o dummy dummy.c
 if [ -x dummy ]
 then echo "g++ compilation OK";
