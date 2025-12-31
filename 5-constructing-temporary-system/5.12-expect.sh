8937b8481043886d1643204b72064bb3e4d9835a
eloevenb <eloevenb@student.42.fr>
2025-12-31 20:31:08 +0100

    5-constructing-temporary-system/

--- 5-constructing-temporary-system/5.12-expect.sh
+++ 5-constructing-temporary-system/5.12-expect.sh
@@ -20,7 +20,7 @@ tarball="expect5.45.4"
 # Generic build steps  #
 ########################
 cd $LFS/sources
-tar -xvf $tarball.tar.xz
+tar -xvf $tarball.tar.gz
 if [ ! -d $LFS/sources/$tarball ]; then
   echo "ERROR: Unable to extract tarball named $tarball, check the file extensions"
   exit 1
